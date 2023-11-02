0df99663480ae5bac534d40ba2ed1c292030b507
Fahri Maulana Al Ghazali <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2023-11-02 19:54:52 +0700

    initial commit

---
+++ Variable/Main.py
@@ -0,0 +1,22 @@
+#variabel adalah tempat menyimpan data
+
+a = 10
+b = 15
+kata = "Memasak"
+
+#pemanggilan variabel
+print(a + b)
+print(kata)
+
+#penamaan variabel panjangg
+nama_saya = "Fahri Maulana"
+nilai_saya = 100
+
+#pemanggilan gabungan variabel
+print("Nama saya ", nama_saya , " dengan nilai ", nilai_saya)
+
+#mengubah nilai
+a = 5
+nama_saya = "Arif Asyam"
+print("nilai A sekarang: ",a)
+print("Nama saya sekarang:",nama_saya)
